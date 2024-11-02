20f0614aa6d3ba2ff77fb0f08359515777f0a069
Ian Valeta <valeta.iac@gmail.com>
2024-11-02 17:18:01 -0400

    installed express and created app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 5000
+
+//app.listen
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+})
+
+//app.get
+//app.post
+//app.put
+//app.delete
+//app.all
+//app.use
